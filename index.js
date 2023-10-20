e5ef0cfce5d128e78df0d4b8812f78231dcf2029
sarkar niloy <sarkarniloy102@gmail.com>
2023-10-20 12:55:14 +0600

    get add product information from product side

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.mypgnvz.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const productCollection = client.db('productDB').collection('product');
+        const userCollection = client.db('productDB').collection('user');
+
+        app.get('/addproduct', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+            
+        })
+        app.post('/addproduct', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('entertainment server is running')
+})
+
+app.listen(port, () => {
+    console.log(`product server is running on port: ${port}`);
+})
